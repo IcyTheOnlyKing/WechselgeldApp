0ab0a3a3ce09da6e7343f72274e095de418d274b
IcyTheKing <mweinmay@htl-steyr.ac.at>
2025-06-30 09:16:20 +0200

    initial commit 2, created StartController.java and start_view.xml as launch point for the app

---
+++ app/src/main/java/htl/steyr/wechselgeldapp/StartController.java
@@ -0,0 +1,13 @@
+package htl.steyr.wechselgeldapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartController extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_view);
+    }
+}
