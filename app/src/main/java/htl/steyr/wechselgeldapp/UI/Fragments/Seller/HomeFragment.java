f08b62e9026f22d78883c257766dc3f552937e16
IcyTheKing <mweinmay@htl-steyr.ac.at>
2025-07-15 10:35:05 +0200

    fixing bugs not ready

--- app/src/main/java/htl/steyr/wechselgeldapp/UI/Fragments/Seller/HomeFragment.java
+++ app/src/main/java/htl/steyr/wechselgeldapp/UI/Fragments/Seller/HomeFragment.java
@@ -4,13 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import htl.steyr.wechselgeldapp.R;
+ import htl.steyr.wechselgeldapp.R;
 import htl.steyr.wechselgeldapp.UI.Fragments.BaseFragment;
 
 public class HomeFragment extends BaseFragment {
